d1c4718f29c4275f353dc3ee6c95d452804b27a7
Kyriakos Barbounakis <k.barbounakis@gmail.com>
2022-05-06 04:46:37 +0000

    handle sync and async methods

--- index.js
+++ index.js
@@ -8,14 +8,29 @@ if (typeof Promise.sequence !== 'function') {
      */
 
     /**
+     * @param {*} value 
+     */
+    function isPromise(value) {
+        return value != null && (typeof value === 'object' || typeof value === 'function') && typeof value.then === 'function';
+    }
+
+    /**
      * Executes an array of promises in series
      * @param {Array<ExecutePromise>} values 
      * @returns {Array<*>}
      */
     Promise.sequence = function promiseSequence(values) {
         return values.reduce(function (promise, item) {
-            return (promise.then(function (result) {
-                return (item().then(Array.prototype.concat.bind(result)));
+            return (promise.then(function (results) {
+                const source = item();
+                // validate a promise-like result
+                if (source != null && (typeof source === 'object' || typeof source === 'function') && typeof source.then === 'function') {
+                    return source.then(function(result) {
+                        // wrap result to an array in order to use it as Array.concat() argument
+                        return [result];
+                    }).then(Array.prototype.concat.bind(results));
+                }
+                return Promise.resolve([source]).then(Array.prototype.concat.bind(results));
             }));
         }, Promise.resolve([]));
     }
--- spec/PromiseSequence.spec.js
+++ spec/PromiseSequence.spec.js
@@ -25,6 +25,61 @@ describe('PromiseSequence', () => {
         expect(results[1]).toEqual(2);
     });
 
+    it('should execute functions in series', async () => {
+        function promise1() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(1);
+                }, 1000)
+            });
+        }
+        function promise2() {
+            return null;
+        }
+        const results = await Promise.sequence([
+            () => promise1(),
+            () => promise2()
+        ]);
+        expect(results).toBeInstanceOf(Array);
+        expect(results[0]).toEqual(1);
+        expect(results[1]).toEqual(null);
+    });
+
+    it('should execute promises and handle array results', async () => {
+        function promise1() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(1);
+                }, 1000)
+            });
+        }
+        function promise2() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve([2, 3]);
+                }, 500)
+            });
+        }
+        const results = await Promise.sequence([
+            () => promise1(),
+            () => promise2()
+        ]);
+        expect(results).toBeInstanceOf(Array);
+        expect(results[0]).toEqual(1);
+        expect(results[1]).toBeInstanceOf(Array);
+        expect(results[1][0]).toEqual(2);
+
+        function functionWithError() {
+            throw new Error('Expected data');
+        }
+
+        await expectAsync(Promise.sequence([
+            () => promise1(),
+            () => functionWithError()
+        ])).toBeRejectedWithError('Expected data');
+
+    });
+
     it('should execute promises in series and handle errors', async () => {
 
         const utils = {
